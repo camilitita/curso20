6d41aaf15041099301f5b2e47017b6f21977faf7
camilitita <128248768+camilitita@users.noreply.github.com>
2024-11-21 22:50:35 -0400

    todolist

---
+++ Javascript/proyectos_seccion3/to-do_list/todo_list.js
@@ -0,0 +1,25 @@
+
+const addtask_input = document.getElementById('input_box');
+const listcontainer = document.getElementById ('list_container');
+const addtask_button = document.getElementById('addtask_button');
+
+function addTask() {
+  const task = addtask_input.value.trim();
+  if (!task) {
+    alert("Please write down a task");
+    return;
+  }
+
+  li.innerHTML = `
+    <label>
+      <input type="checkbox">
+      <span>${task}</span>
+    </label>
+    <span class="edit-btn">Edit</span>
+    <span class="delete-btn">Delete</span>
+    `;
+
+  listcontainer.appendChild(li);
+  addtask_input.value = "";
+}
+addtask_button.addEventListener('click', addTask);
